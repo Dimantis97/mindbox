e20acaffe2f2ced98d6a5cff035252a062d341f8
Dimantis97 <89899992+Dimantis97@users.noreply.github.com>
2022-08-10 11:01:32 +0300

    Create mindbox.sql
    Sql for products and categories

---
+++ mindbox.sql
@@ -0,0 +1,23 @@
+/*Т.к. обычно данные, имеющие связи многие-ко-многим хранятся в 2-х таблицах, связанных промежуточной, создал 3 таблицы:
+prods - продкуты;
+cats - категории;
+links - связующая таблица.*/
+CREATE TABLE prods(
+  id int PRIMARY KEY,
+  prod varchar(10));
+CREATE TABLE cats(
+  id int PRIMARY KEY,
+  cat varchar(10));
+CREATE TABLE links(
+  id int PRIMARY KEY,
+  prod int not NULL, 
+  cat int not NULL, 
+  CONSTRAINT fk_prod FOREIGN KEY(prod) REFERENCES prods(id),
+  CONSTRAINT fk_cat FOREIGN key(cat) REFERENCES cats(id)
+  );
+
+--сам запрос на получение всех пар продукт-категория
+select prods.prod, cats.cat FROM
+links 
+	left JOIN prods on prods.id = links.prod
+    LEFT JOIN cats on cats.id = links.cat;
